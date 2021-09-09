8b1ae3b0e88c632794182b661e93c537aa993e8b
Shreesh30 <shreeshkarthikeyan30@gmail.com>
2021-09-10 05:16:18 +1000

    Login screen and splash screen designed

--- app/src/main/java/com/weet/weet/MainActivity.kt
+++ app/src/main/java/com/weet/weet/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.weet.weet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.splash_animation)
+        val imageView = findViewById<ImageView>(R.id.imageView3)
+
+        imageView.startAnimation(animation)
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
