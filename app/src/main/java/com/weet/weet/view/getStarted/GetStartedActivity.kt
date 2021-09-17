6481ea3bb9bb71f6770334c6939bce53dc454de3
Omer <omerrasheed1995@gmail.com>
2021-09-17 19:34:07 +1000

    - added getStarted activity - add video to get started activity

--- app/src/main/java/com/weet/weet/view/getStarted/GetStartedActivity.kt
+++ app/src/main/java/com/weet/weet/view/getStarted/GetStartedActivity.kt
@@ -1,13 +1,30 @@
 package com.weet.weet.view.getStarted
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 import com.weet.weet.R
 
+
+
+
 class GetStartedActivity : AppCompatActivity() {
+
+    private lateinit var videoView: VideoView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
+
+        configureUI()
+
+    }
+
+    private fun configureUI() {
+        videoView = findViewById<VideoView>(R.id.bgVideoView)
+        val uri = Uri.parse("android.resource://" + packageName + "/" + R.raw.bg_vid)
+        videoView.setVideoURI(uri)
+        videoView.start()
     }
 }
